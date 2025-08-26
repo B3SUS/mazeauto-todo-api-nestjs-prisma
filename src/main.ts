cdc7f2b7adb50a7d47fd2b71a12fc5911792cb36
B3SUS <IB3SUSI@gmail.com>
2025-08-26 14:38:01 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://postman', 'https://mazeauto-frontend-test-508n14ztg-b3sus-projects.vercel.app'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://postman', 'https://mazeauto-frontend-test-508n14ztg-b3sus-projects.vercel.app', 'https://mazeauto-frontend-test-git-master-b3sus-projects.vercel.app'],
     methods: 'GET,POST, PATCH, DELETE',
     credentials: true,
   });
